4583326233020ea1ad4f33ff2c7ffed0aaf8d8da
Marcelo Altmann <altmannmarcelo@users.noreply.github.com>
2018-03-22 19:07:24 -0300

    0.58 rpm version (#101)

--- lib/MHA/ManagerConst.pm
+++ lib/MHA/ManagerConst.pm
@@ -24,7 +24,7 @@ use warnings FATAL => 'all';
 
 use MHA::NodeConst;
 
-our $VERSION          = '0.57';
+our $VERSION          = '0.58';
 our $NODE_MIN_VERSION = '0.54';
 
 our @ALIVE_ERROR_CODES = (
